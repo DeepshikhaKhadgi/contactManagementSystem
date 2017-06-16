837b900d73917a3d26ae2ccc01abd6d247a3edbd
DeepshikhaKhadgi <deepshikhakhadgi@gmail.com>
2017-06-16 11:07:19 -0500

    adding files

---
+++ Models/CMSDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ContactManagementSystem.Models
+{
+    public class CMSDbContext : DbContext
+    {
+        public CMSDbContext()
+            : base("myConnectionString")
+        { 
+        
+        }
+
+        public DbSet<ContactInfoModel> Contacts { get; set; }
+
+
+    }
+}
---
+++ Models/ContactInfoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactManagementSystem.Models
+{
+    public class ContactInfoModel
+    {
+        public int ContactInfoModelId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
